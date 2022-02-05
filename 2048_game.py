5cda81e1d3a5458fc0a1c6d0268decd41a4e74d5
ran93r1210 <junglebeast.934@gmail.com>
2022-02-06 01:49:31 +0530

    Uploading..

--- 2048_game.py
+++ 2048_game.py
@@ -31,6 +31,26 @@ class Board:
         '2048': '#f9f6f2',
     }
 
+    def __init__(self):
+        self.n=4
+        self.window=Tk()
+        self.window.title('ProjectGurukul 2048 Game')
+        self.gameArea=Frame(self.window,bg= 'azure3')
+        self.board=[]
+        self.gridCell=[[0]*4 for i in range(4)]
+        self.compress=False
+        self.merge=False
+        self.moved=False
+        self.score=0
+        for i in range(4):
+            rows=[]
+            for j in range(4):
+                l=Label(self.gameArea,text='',bg='azure4',
+                font=('arial',22,'bold'),width=4,height=2)
+                l.grid(row=i,column=j,padx=7,pady=7)
+                rows.append(l);
+            self.board.append(rows)
+        self.gameArea.grid()
 
 
 
