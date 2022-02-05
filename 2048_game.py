0c7f3767fe5521eba5cb74ea04a2a1cfa242b5a1
ran93r1210 <junglebeast.934@gmail.com>
2022-02-06 01:26:16 +0530

    Uploading..

--- 2048_game.py
+++ 2048_game.py
@@ -1,3 +1,49 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+
+
+class Board:
+    bg_color={
+        '2': '#eee4da',
+        '4': '#ede0c8',
+        '8': '#edc850',
+        '16': '#edc53f',
+        '32': '#f67c5f',
+        '64': '#f65e3b',
+        '128': '#edcf72',
+        '256': '#edcc61',
+        '512': '#f2b179',
+        '1024': '#f59563',
+        '2048': '#edc22e',
+    }
+    color={
+         '2': '#776e65',
+        '4': '#f9f6f2',
+        '8': '#f9f6f2',
+        '16': '#f9f6f2',
+        '32': '#f9f6f2',
+        '64': '#f9f6f2',
+        '128': '#f9f6f2',
+        '256': '#f9f6f2',
+        '512': '#776e65',
+        '1024': '#f9f6f2',
+        '2048': '#f9f6f2',
+    }
+
+
+
+
+
+
+class Game:
+    def __init__(self,gamepanel):
+        self.gamepanel=gamepanel
+        self.end=False
+        self.won=False
+    def start(self):
+        self.gamepanel.random_cell()
+        self.gamepanel.random_cell()
+        self.gamepanel.paintGrid()
+        self.gamepanel.window.bind('<Key>', self.link_keys)
+        self.gamepanel.window.mainloop()
