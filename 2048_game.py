67c1a40b29ca34af2503f380f6a2de70fdbbc808
ran93r1210 <junglebeast.934@gmail.com>
2022-02-06 03:00:14 +0530

    Changes.

--- 2048_game.py
+++ 2048_game.py
@@ -4,35 +4,17 @@ import random
 
 
 class Board:
-    bg_color={
-        '2': '#eee4da',
-        '4': '#ede0c8',
-        '8': '#edc850',
-        '16': '#edc53f',
-        '32': '#f67c5f',
-        '64': '#f65e3b',
-        '128': '#edcf72',
-        '256': '#edcc61',
-        '512': '#f2b179',
-        '1024': '#f59563',
-        '2048': '#edc22e',
-    }
-    color={
-         '2': '#776e65',
-        '4': '#f9f6f2',
-        '8': '#f9f6f2',
-        '16': '#f9f6f2',
-        '32': '#f9f6f2',
-        '64': '#f9f6f2',
-        '128': '#f9f6f2',
-        '256': '#f9f6f2',
-        '512': '#776e65',
-        '1024': '#f9f6f2',
-        '2048': '#f9f6f2',
-    }
-
-
-
+    def __init__(self):
+        self.n=4
+        self.window=Tk()
+        self.window.title('2048 Game')
+        self.gameArea=Frame(self.window,bg= 'azure3')
+        self.board=[]
+        self.gridCell=[[0]*4 for i in range(4)]
+        self.compress=False
+        self.merge=False
+        self.moved=False
+        self.score=0
 
 
 class Game:
@@ -47,4 +29,5 @@ class Game:
         self.gamepanel.window.bind('<Key>', self.link_keys)
         self.gamepanel.window.mainloop()
 gamepanel =Board()
+game2048 = Game( gamepanel)
 game2048.start()
