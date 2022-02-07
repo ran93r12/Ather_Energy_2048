14a3e7c517f792045ad7f533226a0039c67d224c
ran93r1210 <junglebeast.934@gmail.com>
2022-02-07 17:29:37 +0530

    Modification

--- 2048_game.py
+++ 2048_game.py
@@ -1,10 +1,11 @@
-import tkinter as tk
-from tkinter import messagebox
+import tkinter as tk   # GUI module
+# from tkinter import messagebox
 import random
-import copy
+import copy  # to have a matrix copy 
 
 
-def move_up(s, board):
+# to move the grids up and merge them if same in the row or column
+def move_up(s, board):      
     global score
     for c in range(s):
         for j in range(s):
@@ -24,7 +25,7 @@ def move_up(s, board):
                 if(board[i][j] == 0):
                     board[i][j] = board[i+1][j]
                     board[i+1][j] = 0
-    return board
+    return board  # return the updated board
 
 def move_down(s, board):
     global score
@@ -52,7 +53,7 @@ def move_down(s, board):
                     board[i][j] = board[i-1][j]
                     board[i-1][j] = 0
                 i -= 1
-    return board
+    return board  # return the updated board
 
 def move_left(s, board):
     global score
@@ -74,7 +75,7 @@ def move_left(s, board):
                 if(board[i][j] == 0):
                     board[i][j] = board[i][j+1]
                     board[i][j+1] = 0
-    return board
+    return board # return the updated board
 
 def move_right(s, board):
     global score
@@ -102,8 +103,9 @@ def move_right(s, board):
                     board[i][j] = board[i][j-1]
                     board[i][j-1] = 0
                 j -= 1
-    return board
+    return board # return the updated board
 
+# checks whether the full grid is filled with all title or not 
 def check(gamepanel, s, board):
     for i in range(s):
         for j in range(1, s):
@@ -119,6 +121,7 @@ def check(gamepanel, s, board):
     gamepanel.window.unbind("<Key>")
     return 0
 
+# after every move adding values into random cells 
 def spawn(s, board):
     pos = []
     for i in range(s):
@@ -133,6 +136,7 @@ def spawn(s, board):
         board[p[0]][p[1]] = 4
     return board
 
+# for checking whether any grid having the target value i.e 2048
 def do_check_high(s, board, high):
     for i in range(s):
         for j in range(s):
@@ -140,12 +144,12 @@ def do_check_high(s, board, high):
                 return 1
     return 0
 
+# updating grid board for every key press and checks have we reached target or not!
 def update_gridboard(gamepanel, key):
     global board
     global next_board
     global highest
     global prev_board
-    is_undo = 0
     if(key == "Up" or key == "w"):
         next_board = move_up(size, next_board)
     elif(key == "Down" or key == "s"):
@@ -154,24 +158,18 @@ def update_gridboard(gamepanel, key):
         next_board = move_left(size, next_board)
     elif(key == "Right" or key == "d"):
         next_board = move_right(size, next_board)
-    elif(key == "u"):
-        is_undo = 1
-        next_board = copy.deepcopy(prev_board)
-    if(next_board == board):
-        return
-    if(not(is_undo)):
-        prev_board = copy.deepcopy(board)
-        next_board = spawn(size, next_board)
+    next_board = spawn(size, next_board)
     board = copy.deepcopy(next_board)
-    if(check(gamepanel, size, board) == 0):
+    if(check(gamepanel, size, board) == 0):      # checks whether all cells were filled or not
         gamepanel.game_over()
-    if(do_check_high(size, board, highest)):
+    if(do_check_high(size, board, highest)):     # checks whether we hit the target value or not!
         gamepanel.win_game()
         highest *= 2
 
 
 
 class GamePlay():
+    # declaring all the variables need for the GUI purpose
     def __init__(self):
         global score
         self.top_frame_colour = Frame_Color
@@ -186,12 +184,14 @@ class GamePlay():
         self.window = tk.Tk()
         self.window.title("2048 Game")
 
+    # this is displayed when won the game or reach the target
     def win_game(self):
-        new_tile_label = tk.Label(master = self.window, bg = "#000000", fg = "#ff0051", text = str(highest) + " TILE CREATED", font = self.game_font)
+        new_tile_label = tk.Label(master = self.window, bg = "#000000", fg = "#ff0051", text = str(highest) + " Reached SUCCESSFULLY", font = self.game_font)
         new_tile_label.pack(fill = tk.BOTH, expand = True)
         new_tile_label.after(750, new_tile_label.destroy)
         self.game_over()
 
+    # this is used to display when game is finished or over!
     def game_over(self):
         self.window.after(50, self.remove_board_widgets)
         self.window.after(50, self.remove_game_widgets)
@@ -200,6 +200,7 @@ class GamePlay():
         game_over_label.after(2000, game_over_label.destroy)
         self.window.after(2100, self.dashboard)
 
+    # the initial board with start and exit buttons
     def dashboard(self):
         self.nav_widgets = []
         background_frame = tk.Frame(master = self.window, bg = self.top_frame_colour, width = 435, height = 580)
@@ -224,14 +225,17 @@ class GamePlay():
         self.nav_widgets.append(exit_button)
         self.window.mainloop()
 
+    # to remove the dashboard
     def remove_nav_widgets(self):
         for i in self.nav_widgets:
             i.destroy()
 
+    # to start the game 
     def dashboard_start(self):
         self.remove_nav_widgets()
         self.start_game()
 
+    # to remove all the buttons at last
     def remove_game_widgets(self):
         self.top_frame.destroy()
         self.game_label.destroy()
@@ -239,6 +243,7 @@ class GamePlay():
         self.exit_button.destroy()
         self.score_label.destroy()
 
+    # to remove the elements present each one   
     def remove_board_widgets(self):
         for i in self.widgets:
             for j in i:
@@ -246,11 +251,13 @@ class GamePlay():
                 j[1].destroy()
         self.board_frame.destroy()
 
+    # again to start a new 
     def end_start_game(self):
         self.remove_board_widgets()
         self.remove_game_widgets()
         self.start_game()
-
+    
+    # main function the window where the game starts
     def start_game(self):
         self.initialize_board()
         self.top_frame = tk.Frame(master = self.window, bg = self.top_frame_colour)
@@ -276,9 +283,11 @@ class GamePlay():
 
         self.window.bind("<Key>", self.key_press)
 
+    #  close the tkninter window
     def close_window(self):
         exit(0)
 
+    # to create the gridboard of the required size
     def create_gridboard(self):
         self.widgets = []
         for i in range(size):
@@ -290,6 +299,7 @@ class GamePlay():
                 label.pack(fill = tk.BOTH, expand = True)
                 self.widgets[i].append((frame, label))
 
+    # to identify the key press and call necessary functions 
     def key_press(self, event):
         global highest
         g = highest
@@ -297,6 +307,7 @@ class GamePlay():
         update_gridboard(self, key)
         self.update_gridboard()
 
+    # update gridboard and score for every move
     def update_gridboard(self):
         global board
         global next_board
@@ -306,6 +317,7 @@ class GamePlay():
             for j in range(size):
                 self.widgets[i][j][1].config(text = str(board[i][j]), fg = self.foreground_colours[board[i][j]], bg = self.background_colours[board[i][j]])
 
+    # starting initallizing all the variables needed for the game
     def initialize_board(self):
         global board
         global next_board
@@ -313,7 +325,7 @@ class GamePlay():
         global highest
         global score
         board = []
-        highest = 32
+        highest = 2048                # to change the limit of game
         score = 0
         for i in range(size):
             board.append([])
@@ -325,14 +337,28 @@ class GamePlay():
         prev_board = copy.deepcopy(board)
         
     
-
+# the main function which is defaultly called by Python
 if __name__ == "__main__":
+    # colors for tkinter GUI
     Frame_Color = "#faf8ef"
-    Board_Color = "#bbada0"
+    Board_Color = "#92877d"
     text_font_Color = "#2d2f69"
-    ForeGround_Color = {0: "#d6cdc4", 2:"#464646", 4:"#464646", 8:"#ffffff", 16:"#ffffff", 32:"#ffffff", 64:"#ffffff", 128:"#ffffff", 256:"#ffffff", 512:"#ffffff", 1024:"#ffffff", 2048:"#ffffff", 4096:"#ffffff", 8192:"#ffffff", 16384:"#ffffff", 32768:"#ffffff", 65536:"#ffffff", 131072:"#ffffff"}
-    BackGround_Color = {0: "#d6cdc4", 2:"#eee4da", 4:"#ede0c8", 8:"#f2b179", 16:"#f59563", 32:"#f67c5f", 64:"#f65e3b", 128:"#edcf72", 256:"#edcc61", 512:"#edc850", 1024:"#edc53f", 2048:"#edc22e", 4096:"#464646", 8192:"#3d3d3d", 16384:"#19191a", 32768:"#111111", 65536:"#09090a", 131072:"#000000"}
+    ForeGround_Color = {0: "#9e948a", 2: "#776e65", 4: "#776e65", 8: "#f9f6f2", 16: "#f9f6f2",32: "#f9f6f2", 64: "#f9f6f2", 128: "#f9f6f2", 256: "#f9f6f2",512: "#f9f6f2", 1024: "#f9f6f2", 2048: "#f9f6f2"}
+    BackGround_Color = {0: "#9e948a", 2: "#eee4da", 4: "#ede0c8", 8: "#f2b179", 16: "#f59563",32: "#f67c5f", 64: "#f65e3b", 128: "#edcf72", 256: "#edcc61",512: "#edc850", 1024: "#edc53f", 2048: "#edc22e"}
 
-    size = 4
-    gamepanel = GamePlay()
-    gamepanel.dashboard()
+    size = 4                      # to change the grid size we can change here
+    gamepanel = GamePlay()        # object created for the tinketer window
+    gamepanel.dashboard()         # calling the inital function to 
+
+
+
+
+
+
+
+
+
+
+
+
+
