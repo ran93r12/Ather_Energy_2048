98481d9f14c2689c941592c427c92e5f01fa3880
ran93r1210 <junglebeast.934@gmail.com>
2022-02-07 16:21:10 +0530

    Modified

--- 2048_game.py
+++ 2048_game.py
@@ -4,12 +4,8 @@ import random
 
 
 class GamePlay():
-    Frame_Color = "#faf8ef"
-    Board_Color = "#bbada0"
-    ForeGround_Color = {0: "#d6cdc4", 2:"#464646", 4:"#464646", 8:"#ffffff", 16:"#ffffff", 32:"#ffffff", 64:"#ffffff", 128:"#ffffff", 256:"#ffffff", 512:"#ffffff", 1024:"#ffffff", 2048:"#ffffff", 4096:"#ffffff", 8192:"#ffffff", 16384:"#ffffff", 32768:"#ffffff", 65536:"#ffffff", 131072:"#ffffff"}
-    BackGround_Color = {0: "#d6cdc4", 2:"#eee4da", 4:"#ede0c8", 8:"#f2b179", 16:"#f59563", 32:"#f67c5f", 64:"#f65e3b", 128:"#edcf72", 256:"#edcc61", 512:"#edc850", 1024:"#edc53f", 2048:"#edc22e", 4096:"#464646", 8192:"#3d3d3d", 16384:"#19191a", 32768:"#111111", 65536:"#09090a", 131072:"#000000"}
-
     def __init__(self):
+        #colours
         global score
         self.top_frame_colour = Frame_Color
         self.board_frame_colour = Board_Color
@@ -21,18 +17,18 @@ class GamePlay():
         self.score_font = ("Promesh", 10, "bold")
         self.window = tk.Tk()
 
-    def winGame(self):
-        title_label = tk.Label(master = self.window, bg ="#000000", fg = "#ff0051", text = str(highest) + " TILE CREATED", font = self.game_font)
-        title_label.pack(fill = tk.BOTH, expand = True)
-        title_label.after(750,title_label.destory)
-        self.game_Over()
+    def win_game(self):
+        new_tile_label = tk.Label(master = self.window, bg = "#000000", fg = "#ff0051", text = str(highest) + " TILE CREATED", font = self.game_font)
+        new_tile_label.pack(fill = tk.BOTH, expand = True)
+        new_tile_label.after(750, new_tile_label.destroy)
+        self.game_over()
 
-    def gameOver(self):
+    def game_over(self):
         self.window.after(50, self.remove_board_widgets)
         self.window.after(50, self.remove_game_widgets)
-        game_over = tk.Label(master = self.window, text = "GAME OVER!!", font = self.home_game_font, fg = self.board_frame_colour, bg = self.top_frame_colour, width = 20, height = 5)
-        game_over.pack()
-        game_over.after(2000, game_over.destroy)
+        game_over_label = tk.Label(master = self.window, text = "GAME OVER!!", font = self.home_game_font, fg = self.board_frame_colour, bg = self.top_frame_colour, width = 20, height = 5)
+        game_over_label.pack()
+        game_over_label.after(2000, game_over_label.destroy)
         self.window.after(2100, self.dashboard)
 
     def dashboard(self):
@@ -46,7 +42,7 @@ class GamePlay():
         instruction_label = tk.Label(master = background_frame, text = "Use arrow keys or WASD to move tiles\nUse U to undo your last move", fg = self.board_frame_colour, bg = self.top_frame_colour, font = self.board_font)
         instruction_label.pack()
 
-        new_game_button = tk.Button(master = background_frame, text = "New Game", width = 10, height = 3, bg = self.board_frame_colour, fg = "#ffffff", command = self.home_start_game)
+        new_game_button = tk.Button(master = background_frame, text = "New Game", width = 10, height = 3, bg = self.board_frame_colour, fg = "#ffffff", command = self.dashboard_start)
         new_game_button.pack()
 
         exit_button = tk.Button(master = background_frame, text = "Exit", width = 10, height = 3, bg = self.board_frame_colour, fg = "#ffffff", command = self.close_window)
@@ -59,10 +55,109 @@ class GamePlay():
         self.nav_widgets.append(exit_button)
         self.window.mainloop()
 
-    def remove_game_widgets(self):
-        for widgets in self.nav_widgets:
-            widgets.destroy()
+    def remove_nav_widgets(self):
+        for i in self.nav_widgets:
+            i.destroy()
 
-    def starGame(self):
+    def dashboard_start(self):
+        self.remove_nav_widgets()
+        self.start_game()
+
+    def remove_game_widgets(self):
+        self.top_frame.destroy()
+        self.game_label.destroy()
+        self.new_game_button.destroy()
+        self.exit_button.destroy()
+        self.score_label.destroy()
+
+    def remove_board_widgets(self):
+        for i in self.widgets:
+            for j in i:
+                j[0].destroy()
+                j[1].destroy()
+        self.board_frame.destroy()
+
+    def end_start_game(self):
+        self.remove_board_widgets()
         self.remove_game_widgets()
         self.start_game()
+
+    def start_game(self):
+        self.initialize_board()
+        self.top_frame = tk.Frame(master = self.window, bg = self.top_frame_colour)
+        self.top_frame.pack(fill = tk.BOTH, expand = True)
+
+        self.game_label = tk.Label(master = self.top_frame, text = "2048", font = self.game_font, fg = self.board_frame_colour, bg = self.top_frame_colour, width = 4, height = 3)
+        self.game_label.grid(row = 0, column = 0, padx = 3, pady = 3)
+
+
+        self.new_game_button = tk.Button(master = self.top_frame, text = "New Game", width = 9, height = 4, bg = self.board_frame_colour, fg = "#ffffff", command = self.end_start_game)
+        self.new_game_button.grid(row = 0, column = 1, padx = 3, pady = 3)
+
+        self.exit_button = tk.Button(master = self.top_frame, text = "Exit", width = 8, height = 4, bg = self.board_frame_colour, fg = "#ffffff", command = self.close_window)
+        self.exit_button.grid(row = 0, column = 2, padx = 3, pady = 3)
+
+        self.score_label = tk.Label(master = self.top_frame, text = "Score\n" + str(score), font = self.score_font, fg = "#ffffff", bg = self.board_frame_colour, width = 10, height = 3)
+        self.score_label.grid(row = 0, column = 3, padx = 3, pady = 3)
+
+        self.board_frame = tk.Frame(master = self.window, bg = self.board_frame_colour)
+        self.board_frame.pack(fill = tk.BOTH, expand = True)
+
+        self.create_gridboard()
+
+        self.window.bind("<Key>", self.key_press)
+
+    def close_window(self):
+        exit(0)
+
+    def create_gridboard(self):
+        self.widgets = []
+        for i in range(size):
+            self.widgets.append([])
+            for j in range(size):
+                frame = tk.Frame(master = self.board_frame, borderwidth = 1, relief = tk.RAISED)
+                frame.grid(row = i, column = j, padx = 3, pady = 3)
+                label = tk.Label(master = frame, text = str(board[i][j]), font = self.board_font, fg = self.foreground_colours[board[i][j]], bg = self.background_colours[board[i][j]], width = 6, height = 3)
+                label.pack(fill = tk.BOTH, expand = True)
+                self.widgets[i].append((frame, label))
+
+    def key_press(self, event):
+        global highest
+        g = highest
+        key = event.keysym
+        update_gridboard(self, key)
+        self.update_gridboard()
+
+    def update_gridboard(self):
+        global board
+        global next_board
+        global score
+        self.score_label.config(text = "Score\n" + str(score))
+        for i in range(size):
+            for j in range(size):
+                self.widgets[i][j][1].config(text = str(board[i][j]), fg = self.foreground_colours[board[i][j]], bg = self.background_colours[board[i][j]])
+
+    def initialize_board(self):
+        global board
+        global next_board
+        global prev_board
+        global highest
+        global score
+        board = []
+        highest = 32
+        score = 0
+        for i in range(size):
+            board.append([])
+            for j in range(size):
+                board[i].append(0)
+        spawn(size, board)
+        spawn(size, board)
+        next_board = copy.deepcopy(board)
+        prev_board = copy.deepcopy(board)
+        
+    
+
+if __name__ == "__main__":
+    size = 4
+    gamepanel = GamePlay()
+    gamepanel.dashboard()
