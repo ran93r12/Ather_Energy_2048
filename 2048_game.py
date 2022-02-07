34753b721292ba30b75ae6ee1e087a3daf9c140e
ran93r1210 <junglebeast.934@gmail.com>
2022-02-07 15:55:12 +0530

    Modified

--- 2048_game.py
+++ 2048_game.py
@@ -20,3 +20,49 @@ class GamePlay():
         self.home_game_font = ("Courier", 35, "bold")
         self.score_font = ("Promesh", 10, "bold")
         self.window = tk.Tk()
+
+    def winGame(self):
+        title_label = tk.Label(master = self.window, bg ="#000000", fg = "#ff0051", text = str(highest) + " TILE CREATED", font = self.game_font)
+        title_label.pack(fill = tk.BOTH, expand = True)
+        title_label.after(750,title_label.destory)
+        self.game_Over()
+
+    def gameOver(self):
+        self.window.after(50, self.remove_board_widgets)
+        self.window.after(50, self.remove_game_widgets)
+        game_over = tk.Label(master = self.window, text = "GAME OVER!!", font = self.home_game_font, fg = self.board_frame_colour, bg = self.top_frame_colour, width = 20, height = 5)
+        game_over.pack()
+        game_over.after(2000, game_over.destroy)
+        self.window.after(2100, self.dashboard)
+
+    def dashboard(self):
+        self.nav_widgets = []
+        background_frame = tk.Frame(master = self.window, bg = self.top_frame_colour, width = 435, height = 580)
+        background_frame.pack()
+
+        game_label = tk.Label(master = background_frame, text = "2048", font = self.home_game_font, fg = self.board_frame_colour, bg = self.top_frame_colour, width = 10, height = 2)
+        game_label.pack()
+
+        instruction_label = tk.Label(master = background_frame, text = "Use arrow keys or WASD to move tiles\nUse U to undo your last move", fg = self.board_frame_colour, bg = self.top_frame_colour, font = self.board_font)
+        instruction_label.pack()
+
+        new_game_button = tk.Button(master = background_frame, text = "New Game", width = 10, height = 3, bg = self.board_frame_colour, fg = "#ffffff", command = self.home_start_game)
+        new_game_button.pack()
+
+        exit_button = tk.Button(master = background_frame, text = "Exit", width = 10, height = 3, bg = self.board_frame_colour, fg = "#ffffff", command = self.close_window)
+        exit_button.pack()
+
+        self.nav_widgets.append(background_frame)
+        self.nav_widgets.append(game_label)
+        self.nav_widgets.append(instruction_label)
+        self.nav_widgets.append(new_game_button)
+        self.nav_widgets.append(exit_button)
+        self.window.mainloop()
+
+    def remove_game_widgets(self):
+        for widgets in self.nav_widgets:
+            widgets.destroy()
+
+    def starGame(self):
+        self.remove_game_widgets()
+        self.start_game()
